0a445f42f87c3e7fb7db04337872dfe136e442ba
CassBaltz <cass.baltz@gmail.com>
2019-11-22 22:17:33 -0500

    chore(deploy scripts): added eb scripts from green beans

--- backend/express/app.js
+++ backend/express/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 const db = require("../db/models/index");
 const server = require("../graphql/server");
