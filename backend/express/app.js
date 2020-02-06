e696231577854bc2485af3e7857cce4df4d08bd0
CassBaltz <cass.baltz@gmail.com>
2020-02-06 13:32:01 -0500

    chore(start script): update start script for heroku

--- backend/express/app.js
+++ backend/express/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const PORT = process.env.PORT || 4000;
 const app = express();
 const db = require("../db/models/index");
 const server = require("../graphql/server");
@@ -12,15 +11,15 @@ app.use(cookieParser());
 
 
 app.get("/", (req, res) => {
-    console.log("serving index");
     res.sendFile(path.join(__dirname, "./../../frontend/build/index.html"));
 });
 
 app.use(express.static(path.join(__dirname, "./../../frontend/build")));
 
 db.sequelize.authenticate().then(() => {
+    const PORT = process.env.PORT || 4000;
     // run any migrations
     db.sequelize.sync();
     server.applyMiddleware({app});
-    app.listen(PORT, console.log(`currently listening on port ${PORT}`));
+  app.listen(PORT, console.log(`currently listening on port ${PORT}`));
 }).catch(e => {console.log(e);});
